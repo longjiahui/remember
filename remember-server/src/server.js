0beccd1ad60b10d64d9e9f9d8999a966e35c2958
anfo <longjiahui@hotmail.com>
2020-04-06 15:03:50 +0800

    add static dir support

--- remember-server/src/server.js
+++ remember-server/src/server.js
@@ -1,6 +1,8 @@
 const Koa = require('koa');
 const {initAll} = require('koa-serve-decorator');
 const {jwtSecret} = require('./config');
+let staticCache = require('koa-static-cache');
+let LRU = require('lru-cache');
 
 let app = new Koa();
 
@@ -11,6 +13,14 @@ app.use(require('./log'));
 
 //koa-mongo
 app.use(require('./db'));
+
+
+var files = new LRU({ max: 1000 })
+app.use(staticCache({
+  dir: './static',
+  dynamic: true,
+  files: files
+}));
 
 app.use(initAll({
     dirs: [
@@ -23,6 +33,14 @@ app.use(initAll({
         jwtSecret
     },
 }));
+
+//不命中的url全部返回index
+app.use((ctx) =>{
+    if(!ctx.body){
+        ctx.type ='html';
+        ctx.body = require('fs').createReadStream('./static/index.html');
+    }
+})
 
 
 app.listen(3000);
